40b98435bffa7046453701d057e1eb551065b28d
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2024-07-05 00:39:23 +0900

    Fix to check database connection

--- tools/migrate.go
+++ tools/migrate.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 
@@ -11,6 +13,27 @@ import (
 )
 
 func main() {
+	db, err := sql.Open("mysql", "root:mysql@tcp(localhost:3306)/spocon")
+	if err != nil {
+		log.Fatalf("Failed to open database connection: %v", err)
+	}
+	defer db.Close()
+
+	// 最大10回リトライして接続を確認する
+	for i := 0; i < 10; i++ {
+		err = db.Ping()
+		if err == nil {
+			log.Println("Database connection successful.")
+			break
+		}
+		log.Printf("Database connection failed. %v", err)
+		time.Sleep(3 * time.Second)
+	}
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database after multiple attempts: %v", err)
+	}
+
 	m, err := migrate.New(
 		"file://database/migrations",
 		"mysql://root:mysql@tcp(localhost:3306)/spocon",
