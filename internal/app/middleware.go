b47a46175b6480e986a3dc512259bcd0e9eb4749
kumamoto8110icloud.com <kumamoto8110@icloud.com>
2024-08-30 04:55:18 +0900

    Add: APIのレスポンスに含めるステータスコードを定義する基盤作成

--- internal/app/middleware.go
+++ internal/app/middleware.go
@@ -7,9 +7,17 @@ import (
 
 func InitMiddleware(e *echo.Echo) {
 	e.Use(middleware.Recover())
+	e.HTTPErrorHandler = CustomHttpErrorHandler
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:8080"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 	})) // TODO: 動作確認
 	e.Use(middleware.Logger())
+}
+
+func CustomHttpErrorHandler(err error, c echo.Context) {
+	if c.Response().Committed {
+		return
+	}
+	c.JSON(getStatusCode(err), map[string]string{"error": err.Error()})
 }
